ae1f281f4f7d8d112d8b6efae574b1fd61b26cd4
Oleksandr Balan <balan.work.2016@gmail.com>
2023-03-10 11:27:50 -0500

    Initial Commit

---
+++ NSSecureCodingDebug/NSSecureCodingDebug.h
@@ -0,0 +1,18 @@
+//
+//  NSSecureCodingDebug.h
+//  NSSecureCodingDebug
+//
+//  Created by Oleksandr Balan on 2023-03-10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSSecureCodingDebug.
+FOUNDATION_EXPORT double NSSecureCodingDebugVersionNumber;
+
+//! Project version string for NSSecureCodingDebug.
+FOUNDATION_EXPORT const unsigned char NSSecureCodingDebugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSSecureCodingDebug/PublicHeader.h>
+
+
